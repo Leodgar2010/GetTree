f3e8e4ed42167b426cde3cad42de97214c7fe44f
Leodgar2010 <leodgar2010@yandex.ru>
2022-12-22 19:59:50 +0300

    Поигался с итератором и компаратором. Итератор не переписывал. Подумаю на каникулах как сделать интереснее.

--- src/GeoTree.java
+++ src/GeoTree.java
@@ -1,12 +1,15 @@
 import java.io.IOException;
 import java.util.ArrayList;
 import java.io.FileWriter;
+import java.util.Iterator;
+
 public class GeoTree implements Output {
     private ArrayList<Node> tree = new ArrayList<>();
 
     public ArrayList<Node> getTree() {
         return tree;
     }
+
 
     public void parentAppend (Person parent, Person children) throws IOException {
         tree.add(new Node(parent, Relationship.parent, children));
@@ -15,13 +18,10 @@ public class GeoTree implements Output {
     }
 
 
-
-
-
     @Override
     public void outputToFile(String filename, ArrayList n) throws IOException {
         FileWriter fileWriter = new FileWriter(filename);
-        for (int i = 0; i < n.size(); i++) {
+       for (int i = 0; i < n.size(); i++) {
             fileWriter.write(String.valueOf(n.get(i)));
             fileWriter.write ("\n");
         }
